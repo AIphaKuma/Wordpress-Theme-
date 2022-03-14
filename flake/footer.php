35e8dad7db36d130dcb4abcda9f9c957d47a5f8c
AIphaKuma <74193783+AIphaKuma@users.noreply.github.com>
2022-03-14 18:41:30 +0100

    Add files via upload

--- flake/footer.php
+++ flake/footer.php
@@ -1,11 +1,24 @@
     </div>
     <footer>
-        <?php wp_nav_menu([
+        <!-- <?php wp_nav_menu([
             'theme_location' => 'footer',
             'container' => false,
             'menu_class' => 'navbar-nav me-auto mb-2 mb-lg-0'
-        ]) ?>
-    <footer>
+        ]) ?> -->
+
+        <footer class="text-center text-white fixed-bottom" style="background-color: #212529;">
+            <!-- Grid container -->
+            <div class="container p-4"></div>
+            <!-- Grid container -->
+
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                © 2020 Copyright:
+                <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+            </div>
+            <!-- Copyright -->
+        </footer>
+        <footer>
             <?php wp_footer() ?>
             </body>
 
--- flake/front-page.php
+++ flake/front-page.php
@@ -1,7 +1,7 @@
 <?php get_header() ?>
 
 <?php while (have_posts()) : the_post() ?>
-    <h1> <?php the_title(); ?> </h1>
+    <h1 class=" mt-5 "> <?php the_title(); ?> </h1>
     <?php the_content() ?>
     <!-- <a href="<?= get_post_type_archive_link('post') ?>"> Voir la liste des appartements </a> -->
 <?php endwhile; ?>
